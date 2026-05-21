58b97d384c7f9c362d50da190cdc70bd78227160
jonathanmulias <143981160+jonathanmulias@users.noreply.github.com>
2026-05-21 20:47:59 +0700

    game.py

--- Modular/game.py
+++ Modular/game.py
@@ -5,6 +5,11 @@ from jalur import tampilkan_jalur
 from jalur import PANJANG_JALUR
 from leaderboard import skor
 from leaderboard import leaderboard
+from history import simpan_history
+from history import lihat_history
+from stack import push_stack
+from stack import lihat_stack
+
 
 def mulai_game(gmail_login):
     player_pos = 0
@@ -24,6 +29,10 @@ def mulai_game(gmail_login):
         try:
             player = int(input("\nMasukkan angka : "))
 
+            if player == 0:
+                lihat_stack()
+                continue
+
             if player not in kartu_player:
                 print("Angka tidak tersedia!")
                 continue
@@ -39,6 +48,9 @@ def mulai_game(gmail_login):
         kartu_player.remove(player)
         kartu_komputer.remove(komputer)
 
+        old_player = player_pos
+        old_komputer = komputer_pos
+
         if player == komputer:
             print("\nHASIL SERI!")
         elif cek_menang(player, komputer):
@@ -49,6 +61,8 @@ def mulai_game(gmail_login):
             print("\nCOMPUTER MENANG!")
             print(f"Maju {komputer} langkah!")
             komputer_pos += komputer
+            
+        push_stack(player, komputer, old_player, old_komputer)
 
         if player_pos > PANJANG_JALUR:
             player_pos = PANJANG_JALUR
@@ -64,25 +78,68 @@ def mulai_game(gmail_login):
             kartu_komputer = [1,2,3,4,5,6]
             print("Kartu COMPUTER di-reset!")
 
+        game_selesai = False
         if player_pos >= PANJANG_JALUR:
-            print("\nPLAYER MENANG GAME!")
+            simpan_history("MENANG")
+            print("\nPLAYER MENANG GAME!\n")
             hasil_skor = skor(gmail_login)
-            print(f"SKOR KAMU : {hasil_skor}")
+            print("SKOR KAMU : ", hasil_skor)
+            game_selesai = True
 
-            print("\n1. Main Lagi")
-            print("2. Leaderboard")
-            print("3. Kembali ke Menu")
+        elif komputer_pos >= PANJANG_JALUR:
+            simpan_history("KALAH")
+            print("\nKOMPUTER MENANG GAME")
+            game_selesai = True
 
-            pilihan = int(input("Masukkan pilihan : "))
+        # =========================
+        # MENU AKHIR GAME
+        # =========================
+            
+        if game_selesai:
+            while True:
+                print("\n1. Main Lagi")
+                print("2. Leaderboard")
+                print("3. Lihat History")
+                print("4. Kembali ke Menu")
 
-            if pilihan == 1:
-                player_pos = 0
-                komputer_pos = 0
-                kartu_player = [1,2,3,4,5,6]
-                kartu_komputer = [1,2,3,4,5,6]
-                print("\nGAME DIMULAI ULANG!")
-            elif pilihan == 2:
-                leaderboard()
-                break
-            elif pilihan == 3:
-                break
+                try:
+                    pilihan = int(input("Masukkan pilihan : "))
+                except ValueError:
+                    print("Input harus angka!")
+                    continue
+
+                # =========================
+                # MAIN LAGI
+                # =========================
+                if pilihan == 1:
+
+                    player_pos = 0
+                    komputer_pos = 0
+
+                    kartu_player = [1,2,3,4,5,6]
+                    kartu_komputer = [1,2,3,4,5,6]
+
+                    print("\nGAME DIMULAI ULANG!")
+
+                    break
+
+                # =========================
+                # LEADERBOARD
+                # =========================
+                elif pilihan == 2:
+                    leaderboard()
+
+                # =========================
+                # HISTORY
+                # =========================
+                elif pilihan == 3:
+                    lihat_history()
+
+                # =========================
+                # KEMBALI KE MENU
+                # =========================
+                elif pilihan == 4:
+                    return
+
+                else:
+                    print("Pilihan tidak tersedia!")
